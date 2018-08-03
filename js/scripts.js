3c3193543250536d3e25d2b6451c0130b1a932f3
Connie <clai011@ucr.edu>
2018-08-03 05:33:12 -0700

    need to test

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 
             //HOME PAGE TYPE FUNCTION//
             var i = 0;
-            var txt = "I'm Connie. Front-end web developer. Based in Fresno, CA.";
+            var txt = "A front-end web developer, based in Fresno, CA.";
             var speed = 180;
             
             function type() {
